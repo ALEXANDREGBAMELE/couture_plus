7ac8d4ac0869d4c98f8425d0de383240e38b3e97
Alexandre GBAMELE <gbamelealexandre02@gmai.com>
2026-02-27 00:39:15 +0100

    ajout du splashScreen et logo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,45 +1,66 @@
 import { Colors } from "@/constants/theme";
 import { initTables } from "@/database/orderRepository";
 import { Stack } from "expo-router";
-import { useEffect } from "react";
+import * as SplashScreen from "expo-splash-screen";
+import { useCallback, useEffect, useState } from "react";
+import { View } from "react-native";
+
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+
+  const [appIsReady, setAppIsReady] = useState(false);
+
   useEffect(() => {
-    // resetDatabase();
-    initTables();
+    async function prepare() {
+      try {
+
+        // 🔥 On attend que SQLite soit initialisé
+        await initTables();
+
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
   }, []);
 
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: Colors.light.tint,
-        },
-        headerTintColor: "#fff",
-        headerTitleStyle: {
-          fontWeight: "600",
-        },
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{ headerShown: false }}
-      />
-
-      <Stack.Screen
-        name="create"
-        options={{ title: "Nouvelle commande" }}
-      />
-
-      <Stack.Screen
-        name="orders/[id]"
-        options={{ title: "Détails commande" }}
-      />
-
-      <Stack.Screen
-        name="settings"
-        options={{ title: "Paramètres" }}
-      />
-    </Stack>
+    <View style={{ flex: 1 }} onLayout={onLayoutRootView}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: Colors.light.tint,
+          },
+          headerTintColor: "#fff",
+          headerTitleStyle: {
+            fontWeight: "600",
+          },
+        }}
+      >
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="create" options={{ title: "Nouvelle commande" }} />
+        <Stack.Screen name="orders/[id]" options={{ title: "Détails commande" }} />
+        <Stack.Screen name="settings" options={{ title: "Paramètres" }} />
+      </Stack>
+    </View>
   );
 }
